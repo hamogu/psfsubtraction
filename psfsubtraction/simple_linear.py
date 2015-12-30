6061cb4da2355e2cfbb65ca7311c350481142016
hamogu <hgunther@mit.edu>
2015-12-30 14:20:25 -0500

    use more intelligent grouping to reduce run time

--- psfsubtraction/simple_linear.py
+++ psfsubtraction/simple_linear.py
@@ -1,4 +1,5 @@
 from itertools import imap, ifilterfalse
+from collections import defaultdict
 from warnings import warn
 import numpy as np
 
@@ -20,6 +21,18 @@ class SimpleLinearSubtraction(PSFSubtraction):
 
 
 def mask_except_pixel(self, pix):
+    '''Helper function - make True/False mask that is True at pix
+
+    Parameters
+    ----------
+    pix : int
+        Index in flattened image
+
+    Returns
+    -------
+    m : np.array of bool
+        Array that is True at and only at position ``pix``.
+    '''
     m = self.dim2to1(np.zeros_like(self.image, dtype=bool))
     m[pix] = True
     return m
@@ -29,51 +42,70 @@ def pixel_by_pixel(self):
     '''Each pixel it its own region.
 
     This is an extreme LOCI variant, where each region is made up of a single
-    pixel only. However, for images with even a few thousand pixels that is
-    prohibitive in run time.
+    pixel only.
+    This function returns one region per unmasked image pixel.
+
+    Note
+    ----
+    Even for images with just a few thousand pixels this method is
+    too expensive in run time.
+
+    Returns
+    -------
+    regions : iterator
+        True/False index arrays
     '''
     # image_unmasked returns a list with one element.
     # All pixels to be used are marked True.
     mask = image_unmasked(self)[0]
 
-    return imap(mask_except_pixel, ifilterfalse(lambda x: mask[x], range(len(mask))))
+    return imap(mask_except_pixel,
+                ifilterfalse(lambda x: mask[x], range(len(mask))))
 
 
-# Since pixel_by_pixel is far too slow, we need a more intelligent scheme to divide
-# the image into regions.
-# The vast majority of pixels is NOT masked in any image. Process those in one chunk.
+def group_by_basis(self):
+    '''Group pixels with the same valid bases into one region.
 
-def all_plus_pixel_by_pixel(self):
-    '''Simple all-at-one treatment with masked pixel special handling.
+    For each valid pixel in the image, this function checks which bases
+    are valid at that pixel.
+    It then groups pixels with the same valid bases into one region.
 
-    All pixels that are unmasked in every image are treated at once (for speed),
-    pixels that are masked in some of the images receive special treatment.
+    Theoretically, there could be 2^60 combinations of bases for 60 bases,
+    but in practice the valid pixels in the bases are not randomly
+    distributed, so that typically a much smaller number of regions is
+    generated.
 
-    reads self.min_usable_bases
+    Returns
+    -------
+    regions : list of index arrays
     '''
-    masked_somewhere = self.image1d.mask | (np.sum(self.psfbase1d.mask, axis=1) >=1)
-    masked_in_too_many_bases = np.sum(self.psfbase1d.mask, axis=1) > self.min_usable_bases
+    imagemask = np.ma.getmaskarray(self.image1d)
+    basemask = np.ma.getmaskarray(self.psfbase1d)
 
-    regionlist = [~masked_somewhere]
+    D = defaultdict(list)
+    for i in range(imagemask.shape[0]):
+        # Add to the dict UNLESS the image itself is masked.
+        if not imagemask[i]:
+            D[tuple(basemask[i, :])].append(i)
 
-    pixelstoadd = masked_somewhere & ~self.image1d.mask & ~masked_in_too_many_bases
-    for pix in np.nonzero(pixelstoadd)[0]:
-        regionlist.append(mask_except_pixel(self, pix))
-
-    return regionlist
+    return D.values()
 
 
 class ExtremeLOCI(PSFSubtraction):
 
     fitpsfcoeff = psf_from_projection
-    regions = all_plus_pixel_by_pixel
+    regions = group_by_basis
     min_usable_bases = 35
 
     def findbase(self, region):
         '''Return all bases that are not masked in any pixel in region'''
         indbase = ~np.ma.getmaskarray(self.psfbase1d)[region, :]
         # region could have several pixels in it.
-        if region.sum() == 0:
+        # region could be
+        # - np.array/list/tuple of True / False
+        # - np.array/list/tuple of index numbers
+        check = np.asanyarray(region)
+        if (check.dtype == bool and check.sum() == 0) or (len(check) == 0):
             raise ValueError('The input region selects no pixel.')
         else:
             return np.min(indbase, axis=0)
@@ -83,14 +115,16 @@ class ExtremeLOCI(PSFSubtraction):
 
         The region is maximal in the sense that it includes all pixels that are
         not masked in the data or any of the bases.
+
+        Returns
+        -------
+        fitregion : np.array of type bool
+            True for those pixels that should be included in the fit
         '''
-        psfmask = np.ma.getmaskarray(self.psfbase1d.mask[:, indpsf])
-        fitregion = np.max(psfmask, axis=1)
-        fitregion *= np.ma.getmaskarray(self.image1d)
-        # Go from masked=True, to an index array that is True where no mask is set
-        fitregion = ~fitregion
+        psfmask = np.max(np.ma.getmaskarray(self.psfbase1d[:, indpsf]), axis=1)
+        datamask = np.ma.getmaskarray(self.image1d)
+        fitregion = ~psfmask & ~datamask
+
         if fitregion.sum() <= (indpsf != False).sum():
             warn('Fit underdetermined. Choose larger fit region or smaller base.')
         return fitregion
-
-        fitpsfcoeff = psf_from_projection
