c516aef09f5437f8f24c8a0cfa319e83c50f46e7
hamogu <moritz.guenther@gmx.de>
2016-01-13 20:55:14 -0500

    do a few imports in __init__

--- psfsubtraction/__init__.py
+++ psfsubtraction/__init__.py
@@ -12,4 +12,4 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from psfsubtraction import *
+    from psfsubtraction import fitters, regions, optregion, findbase, fitpsf, prepare
